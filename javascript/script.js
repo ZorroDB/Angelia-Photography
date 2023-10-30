a1dc4db9028ac7f857ba866e86ddbef2c8406d3e
diego <diegoblom01@gmail.com>
2023-10-30 18:37:24 +0100

    modal, broken

--- javascript/script.js
+++ javascript/script.js
@@ -21,3 +21,20 @@ if (n != undefined) {
 }
 
 var timer = setInterval(ChangeSlide, interval);
+
+
+var modal = document.getElementById("myModal");
+
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  console.log("successfully showing image");
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
